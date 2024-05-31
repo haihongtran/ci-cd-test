df317b351a31f49ea29b413894108b39b886c827
Kevin Tran <seanap@asleep.ai>
2024-05-31 18:42:42 +0900

    Add more fail and error test cases

--- tests/test.py
+++ tests/test.py
@@ -9,12 +9,74 @@ class UnitTest(TestCase):
         # Pass
         self.assertEqual(numpy.array([1, 2, 3]).sum(), 6)
 
-    @unittest.SkipTest
     def test_fail(self):
         # Fail
         self.fail("Intended fail test.")
 
-    @unittest.SkipTest
+    def test_fail_2(self):
+        # Fail
+        self.fail("Intended fail test.")
+
+    def test_fail_3(self):
+        # Fail
+        self.fail("Intended fail test.")
+
+    def test_fail_4(self):
+        # Fail
+        self.fail("Intended fail test.")
+
+    def test_fail_5(self):
+        # Fail
+        self.fail("Intended fail test.")
+
+    def test_fail_6(self):
+        # Fail
+        self.fail("Intended fail test.")
+
+    def test_fail_7(self):
+        # Fail
+        self.fail("Intended fail test.")
+
+    def test_fail_8(self):
+        # Fail
+        self.fail("Intended fail test.")
+
+    def test_fail_9(self):
+        # Fail
+        self.fail("Intended fail test.")
+
     def test_error(self):
         # Error
         raise ValueError("Intended error test.")
+
+    def test_error_2(self):
+        # Error
+        raise ValueError("Intended error test.")
+
+    def test_error_3(self):
+        # Error
+        raise ValueError("Intended error test.")
+
+    def test_error_5(self):
+        # Error
+        raise ValueError("Intended error test.")
+
+    def test_error_6(self):
+        # Error
+        raise ValueError("Intended error test.")
+
+    def test_error_7(self):
+        # Error
+        self.assertEqual(4 / 0, 0)
+
+    def test_error_8(self):
+        # Error
+        self.assertEqual(8 / 0, 0)
+
+    def test_error_9(self):
+        # Error
+        self.assertEqual(9 / 0, 0)
+
+    def test_error_0(self):
+        # Error
+        self.assertEqual(0 / 0, 0)
